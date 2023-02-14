6977d85ab80ffa3147ffa3f3a033754e00cbdfff
roaxen <112213195+roaxen@users.noreply.github.com>
2023-02-14 15:28:25 +0100

    Kotlin

--- HomelessHelper/HomelessHelper/src/main/java/dao/AboutusJpaSpring.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.Aboutus;
-
-public interface AboutusJpaSpring extends JpaRepository<Aboutus,Integer>{
-}
